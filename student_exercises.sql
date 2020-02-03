9cfd54ef973ba61355b970d8f93884b69cede430
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-03 11:33:20 -0600

    created tables for studentexercises.db and added code to student_exercises.sql

---
+++ student_exercises.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS Cohorts;
+DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Instructors;
+DROP TABLE IF EXISTS Exercises;
+DROP TABLE IF EXISTS Student_Exercises;
+
+CREATE TABLE Cohorts (
+	'Cohort_Id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'Name' TEXT NOT NULL UNIQUE
+);
+
+
+CREATE TABLE Students (
+	'Student_Id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'First_Name' TEXT NOT NULL,
+	'Last_Name' TEXT NOT NULL,
+	'Slack_Handle' TEXT NOT NULL UNIQUE,
+	'Cohort_Id' INTEGER,
+	FOREIGN KEY('Cohort_Id') REFERENCES 'Cohorts'('Cohort_Id')
+);
+
+
+CREATE TABLE Instructors (
+	'Instructor_Id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'First_Name' TEXT NOT NULL,
+	'Last_Name' TEXT NOT NULL,
+	'Slack_Handle' TEXT NOT NULL UNIQUE,
+	'Speciality' TEXT,
+	'Cohort_Id' INTEGER,
+	FOREIGN KEY('Cohort_Id') REFERENCES 'Cohorts'('Cohort_Id')
+);
+
+
+CREATE TABLE Exercises (
+	'Exercise_Id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'Exercise_Name' TEXT NOT NULL UNIQUE,
+	'Exercise_Language' TEXT NOT NULL
+);
+
+
+CREATE TABLE Student_Exercises (
+	'Student_Exercise_Id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	'Student_Id' INTEGER NOT NULL,
+	'Exercise_Id' INTEGER NOT NULL,
+	FOREIGN KEY('Student_Id') REFERENCES 'Students'('Student_Id'),
+	FOREIGN KEY('Exercise_Id') REFERENCES 'Exercises'('Exercise_Id')
+);
+
